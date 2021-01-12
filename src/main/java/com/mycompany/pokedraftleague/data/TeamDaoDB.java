b038019c1ed8a24577270bbaf6b989bca6310cd1
Isaac Rezey <isaacrez20@gmail.com>
2021-01-12 15:51:54 -0500

    Properly incorporate Coach object to CRUD methods

--- src/main/java/com/mycompany/pokedraftleague/data/TeamDaoDB.java
+++ src/main/java/com/mycompany/pokedraftleague/data/TeamDaoDB.java
@@ -5,7 +5,9 @@
  */
 package com.mycompany.pokedraftleague.data;
 
+import com.mycompany.pokedraftleague.data.CoachDaoDB.CoachMapper;
 import com.mycompany.pokedraftleague.data.PokemonDaoDB.PokemonMapper;
+import com.mycompany.pokedraftleague.models.Coach;
 import com.mycompany.pokedraftleague.models.Lineup;
 import com.mycompany.pokedraftleague.models.Pokemon;
 import com.mycompany.pokedraftleague.models.Team;
@@ -35,7 +37,9 @@ public class TeamDaoDB implements TeamDao {
     @Override
     public List<Team> getAllTeams() {
         final String GET_ALL_TEAMS = "SELECT * FROM team";
-        return jdbc.query(GET_ALL_TEAMS, new TeamMapper());
+        List<Team> teams = jdbc.query(GET_ALL_TEAMS, new TeamMapper());
+        addCoachToTeams(teams);
+        return teams;
     }
 
     @Override
@@ -43,7 +47,9 @@ public class TeamDaoDB implements TeamDao {
         final String GET_TEAM_BY_LEAGUE_ID = "SELECT t.* FROM team AS t "
                 + "JOIN leagueteam AS lt ON t.id = lt.teamId "
                 + "WHERE lt.leagueId = ?";
-        return jdbc.query(GET_TEAM_BY_LEAGUE_ID, new TeamMapper(), leagueId);
+        List<Team> teams = jdbc.query(GET_TEAM_BY_LEAGUE_ID, new TeamMapper(), leagueId);
+        addCoachToTeams(teams);
+        return teams;
     }
 
     @Override
@@ -60,7 +66,9 @@ public class TeamDaoDB implements TeamDao {
     public Team getTeamById(int id) {
         try {
             final String GET_TEAM_BY_ID = "SELECT * FROM team WHERE id = ?";
-            return jdbc.queryForObject(GET_TEAM_BY_ID, new TeamMapper(), id);
+            Team team = jdbc.queryForObject(GET_TEAM_BY_ID, new TeamMapper(), id);
+            addCoachToTeam(team);
+            return team;
         } catch (DataAccessException e) {
             return null;
         }
@@ -68,11 +76,12 @@ public class TeamDaoDB implements TeamDao {
 
     @Override
     public Team addTeam(Team team) {
-        final String ADD_TEAM = "INSERT INTO team (name, acronym) "
-                + "VALUES (?, ?)";
+        final String ADD_TEAM = "INSERT INTO team (name, acronym, coachId) "
+                + "VALUES (?, ?, ?)";
         jdbc.update(ADD_TEAM,
                 team.getName(),
-                team.getAcronym());
+                team.getAcronym(),
+                team.getCoach().getId());
         
         int newId = jdbc.queryForObject("SELECT LAST_INSERT_ID()", Integer.class);
         team.setId(newId);
@@ -96,6 +105,23 @@ public class TeamDaoDB implements TeamDao {
     public void deleteTeamById(int id) {
         final String DELETE_TEAM_BY_ID = "DELETE FROM team WHERE id = ?";
         jdbc.update(DELETE_TEAM_BY_ID, id);
+    }
+    
+    private void addCoachToTeam(Team team) {
+        team.setCoach(getCoachForTeam(team));
+    }
+    
+    private void addCoachToTeams(List<Team> teams) {
+        for (Team team : teams) {
+            addCoachToTeam(team);
+        }
+    }
+    
+    private Coach getCoachForTeam(Team team) {
+        final String GET_COACH_FOR_TEAM = "SELECT c.* FROM coach AS c"
+                + "JOIN team AS t ON t.coachId = c.id "
+                + "WHERE t.id = ?";
+        return jdbc.queryForObject(GET_COACH_FOR_TEAM, new CoachMapper(), team.getId());
     }
     
     public static final class TeamMapper implements RowMapper<Team> {
