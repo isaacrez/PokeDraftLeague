19792c1fe98403ed9aa4fa89fae4a6680f0aef4f
Isaac Rezey <isaacrez20@gmail.com>
2021-01-21 13:50:03 -0500

    Enable request requests for local testing

--- src/main/java/com/mycompany/pokedraftleague/controller/MatchController.java
+++ src/main/java/com/mycompany/pokedraftleague/controller/MatchController.java
@@ -12,6 +12,7 @@ import com.mycompany.pokedraftleague.models.MatchResults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author isaacrez
  */
 @RestController
+@CrossOrigin("http://localhost:3000/")
 @RequestMapping("/api/match")
 public class MatchController {
     
--- src/main/java/com/mycompany/pokedraftleague/controller/PokemonController.java
+++ src/main/java/com/mycompany/pokedraftleague/controller/PokemonController.java
@@ -8,6 +8,7 @@ package com.mycompany.pokedraftleague.controller;
 import com.mycompany.pokedraftleague.data.PokemonDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author isaacrez
  */
 @RestController
+@CrossOrigin("http://localhost:3000/")
 @RequestMapping("/api/pokemon")
 public class PokemonController {
     
