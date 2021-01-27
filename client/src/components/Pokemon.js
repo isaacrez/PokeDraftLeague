392b78139efe4e3d7a350c7187f3572d543d4018
Isaac Rezey <isaacrez20@gmail.com>
2021-01-27 14:59:50 -0500

    Extract ToggleBar from Pokemon file

--- client/src/components/Pokemon.js
+++ client/src/components/Pokemon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import TableNavBar from './TableNavBar';
 import PokeTable from './PokeTable';
-import ToggleButton from './ToggleButton';
+import ToggleBar from './ToggleBar';
 
 function Pokemon() {
     const [page, setPage] = React.useState(1);
@@ -13,40 +13,12 @@ function Pokemon() {
         "League Stats": false
     });
 
-    function updateDisplay(key, val) {
-        setOnDisplay(prevState => {
-            let newState = {...prevState}
-            newState[key] = val;
-            return newState;
-        });
-    }
-
-    function generateSwitches() {
-        let switches = [];
-        for (let label in onDisplay) {
-            switches.push(
-                <div className="d-flex flex-column align-items-center"
-                    key={label}>
-                    <ToggleButton for={label}
-                        checked={onDisplay[label]}
-                        toggle={(val) => updateDisplay(label, val)} />
-                    <label htmlFor={label}>{label}</label>
-                </div>
-            )
-        }
-        return switches;
-    }
-
     return (
         <div className="full-stripe">
-            
-            <form className="w-100 d-flex justify-content-between align-items-center mb-2">
-                {generateSwitches()}
-                <button className="btn btn-secondary h-75"
-                    type="button">
-                    Display
-                </button>
-            </form>
+            <ToggleBar 
+                state={onDisplay} 
+                setState={setOnDisplay}
+                btnLabel="Display" />
 
             <PokeTable 
                 page={page}
---
+++ client/src/components/ToggleBar.js
@@ -0,0 +1,40 @@
+import React from '../../node_modules/react';
+import ToggleButton from './ToggleButton';
+
+function ToggleBar(props) {
+    function updateState(key, val) {
+        props.setState(prevState => {
+            let newState = {...prevState}
+            newState[key] = val;
+            return newState;
+        });
+    }
+
+    function generateSwitches() {
+        let switches = [];
+        for (let label in props.state) {
+            switches.push(
+                <div className="d-flex flex-column align-items-center"
+                    key={label}>
+                    <ToggleButton for={label}
+                        checked={props.state[label]}
+                        toggle={(val) => updateState(label, val)} />
+                    <label htmlFor={label}>{label}</label>
+                </div>
+            )
+        }
+        return switches;
+    }
+
+    return (
+        <form className="w-100 d-flex justify-content-between align-items-center mb-2">
+            {generateSwitches()}
+            <button className="btn btn-secondary h-75"
+                type="button">
+                {props.btnLabel}
+            </button>
+        </form>
+    )
+}
+
+export default ToggleBar;
