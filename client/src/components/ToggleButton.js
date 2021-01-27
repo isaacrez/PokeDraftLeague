76259aeadcb7f4c13e4bc68c6b6134be3fd97cb2
Isaac Rezey <isaacrez20@gmail.com>
2021-01-27 13:12:01 -0500

    Create ToggleButton

---
+++ client/src/components/ToggleButton.js
@@ -0,0 +1,12 @@
+import React from '../../node_modules/react';
+
+function ToggleButton() {
+    return (
+        <label class="switch">
+            <input type="checkbox" />
+            <span class="slider round"></span>
+        </label>
+    )
+}
+
+export default ToggleButton;
