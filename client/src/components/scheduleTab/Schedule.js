a9f5fdccddb44da300235a3a3d2070ec7d3bfa12
Isaac Rezey <isaacrez20@gmail.com>
2021-02-11 20:16:56 -0500

    Schedule data displays

--- client/src/components/scheduleTab/Schedule.js
+++ client/src/components/scheduleTab/Schedule.js
@@ -1,31 +1,53 @@
 import React from 'react';
 
 function Schedule(props) {
-    return props.league !== "" ?
-        regularSchedule(props) : emptySchedule();
+    return props.league.id === -1 ?
+        <EmptySchedule /> :
+        <RegularSchedule league={props.league} />;
 }
 
-function regularSchedule(props) {
+function RegularSchedule(props) {
+    const [matches, setMatches] = React.useState([]);
+
+    React.useEffect(() => {
+        let url = "http://localhost:8080/api/match/schedule/" + props.league.id;
+        fetch(url, {type: "GET"})
+            .then(response => response.json())
+            .then(matchData => setMatches(matchData))
+            .catch(error => console.log(error));
+    }, [])
+
+    function makeTableContent() {
+        return matches.map(match => 
+            <tr>
+                <td>{match.scheduledWeek}</td>
+                <td>{match.teams[0].name}</td>
+                <td>{match.teams[1].name}</td>
+            </tr>
+        );
+    }
+
     return (
         <div className="full-stripe">
             <h1>Match Schedule</h1>
-            <h2>{props.league}</h2>
+            <h2>{props.league.name}</h2>
             <table className="w-100 table table-dark table-striped">
                 <thead>
                     <tr>
                         <th>Week</th>
-                        <th colSpan="2">Team</th>
-                        <th colSpan="2">Team</th>
+                        <th>Team</th>
+                        <th>Team</th>
                     </tr>
                 </thead>
                 <tbody>
+                    {makeTableContent()}
                 </tbody>
             </table>
         </div>
     )
 }
 
-function emptySchedule() {
+function EmptySchedule() {
     return (
         <div className="full-stripe">
             <h1>Match Schedule... is Missing!</h1>
