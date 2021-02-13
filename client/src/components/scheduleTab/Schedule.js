5b7f40b3193adc9a2afaf7baf5c21f6ffcb9231e
Isaac Rezey <isaacrez20@gmail.com>
2021-02-12 22:13:47 -0500

    Add scaffold for TeamSelector

--- client/src/components/scheduleTab/Schedule.js
+++ client/src/components/scheduleTab/Schedule.js
@@ -15,6 +15,7 @@ function Schedule(props) {
 function RegularSchedule(props) {
     const [matches, setMatches] = React.useState([]);
     const [week, setWeek] = React.useState(0);
+    const [team, setTeam] = React.useState("");
 
     React.useEffect(() => {
         let url = "http://localhost:8080/api/match/schedule/" + props.league.id;
@@ -24,16 +25,28 @@ function RegularSchedule(props) {
             .catch(error => console.log(error));
     }, [props.league.id])
 
+    function makeTeamOptions() {
+        return <option>All</option>
+    }
 
     return (
         <div className="full-stripe">
             <h1>Match Schedule</h1>
             <h2>{props.league.name}</h2>
 
-            <WeekSelector
-                setWeek={setWeek}
-                matches={matches}
-                NO_WEEK_SELECT={NO_WEEK_SELECT} />
+            <div className="w-100 d-flex justify-content-between">
+                <WeekSelector
+                    setWeek={setWeek}
+                    matches={matches}
+                    NO_WEEK_SELECT={NO_WEEK_SELECT} />
+
+                <div className="d-flex minor-dropdown w-25 justify-content-around mb-3">
+                    <label htmlFor="team">Team</label>
+                    <select id="team" onChange={e => props.setTeam(e.target.value)}>
+                        {makeTeamOptions()}
+                    </select>
+                </div>
+            </div>
 
             <TableBody 
                 matches={matches}
