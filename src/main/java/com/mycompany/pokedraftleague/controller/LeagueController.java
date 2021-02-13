150d9e9ca5c669a3749a00f44328524e13a7740d
Isaac Rezey <isaacrez20@gmail.com>
2021-02-12 22:35:40 -0500

    Add API endpoint for getting league by ID

--- src/main/java/com/mycompany/pokedraftleague/controller/LeagueController.java
+++ src/main/java/com/mycompany/pokedraftleague/controller/LeagueController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +33,10 @@ public class LeagueController {
     @GetMapping()
     public ResponseEntity getAllLeagues() {
         return ResponseEntity.ok(leagueDao.getAllLeagues());
+    }
+    
+    @GetMapping("/{leagueId}")
+    public ResponseEntity getLeagueById(@PathVariable int leagueId) {
+        return ResponseEntity.ok(leagueDao.getLeagueById(leagueId));
     }
 }
