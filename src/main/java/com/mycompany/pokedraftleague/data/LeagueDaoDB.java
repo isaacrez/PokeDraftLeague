1fc270a554e020655de594720d79114dcbcab77b
Isaac Rezey <isaacrez20@gmail.com>
2021-02-12 22:33:54 -0500

    Add Teams to League selection

--- src/main/java/com/mycompany/pokedraftleague/data/LeagueDaoDB.java
+++ src/main/java/com/mycompany/pokedraftleague/data/LeagueDaoDB.java
@@ -10,6 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -22,21 +23,42 @@ import org.springframework.stereotype.Repository;
 public class LeagueDaoDB implements LeagueDao {
     
     private final JdbcTemplate jdbc;
+    private final TeamDao teamDao;
     
     @Autowired
-    public LeagueDaoDB(JdbcTemplate jdbcTemplate) {
+    public LeagueDaoDB(JdbcTemplate jdbcTemplate, TeamDao teamDao) {
         this.jdbc = jdbcTemplate;
+        this.teamDao = teamDao;
     }
 
     @Override
     public List<League> getAllLeagues() {
         final String GET_ALL_LEAGUES = "SELECT * FROM league";
-        return jdbc.query(GET_ALL_LEAGUES, new LeagueMapper());
+        List<League> leagues = jdbc.query(GET_ALL_LEAGUES, new LeagueMapper());
+        addTeamsToLeague(leagues);
+        return leagues;
     }
 
     @Override
     public League getLeagueById(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            final String GET_LEAGUE_BY_ID = "SELECT * FROM league WHERE id = ?";
+            League league = jdbc.queryForObject(GET_LEAGUE_BY_ID, new LeagueMapper(), id); 
+            addTeamsToLeague(league);
+            return league;
+        } catch (DataAccessException e) {
+            return null;
+        }
+    }
+    
+    private void addTeamsToLeague(List<League> leagues) {
+        for (League league : leagues) {
+            addTeamsToLeague(league);
+        }
+    }
+    
+    private void addTeamsToLeague(League league) {
+        league.setTeams(teamDao.getAllTeamsForLeague(league.getId()));
     }
 
     @Override
