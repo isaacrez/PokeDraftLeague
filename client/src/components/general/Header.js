0868a30b12f6847fcd610bd6210d9eeab7ff6ff9
Isaac Rezey <isaacrez20@gmail.com>
2021-02-19 13:20:14 -0500

    Improve header styling

--- client/src/components/general/Header.js
+++ client/src/components/general/Header.js
@@ -30,15 +30,15 @@ function Header(props) {
     return (
         <header>
             <nav className="flex flex-column">
-                <div className="w-100 d-flex align-items-center">
+                <div className="w-100 d-flex justify-content-between align-items-center">
                     <button onClick={() => setVisible(v => !v)}
                         className="icon m-1">
                         <img src={`${process.env.PUBLIC_URL}/img/ui/tripleLine.png`} />
                     </button>
 
-                    <h1 className="justify-self-center">{props.league.name}</h1>
+                    <h1>{props.league.name}</h1>
 
-                    <h2 className="ml-3">{props.mainContent}</h2>
+                    <h2 className="mr-3">{props.mainContent}</h2>
                 </div>
 
                 {visible && buildDropdown()}
