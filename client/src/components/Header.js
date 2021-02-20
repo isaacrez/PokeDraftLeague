5ad594afe2eb29b3ccce997d285f642daa16725e
Isaac Rezey <isaacrez20@gmail.com>
2021-02-19 22:23:47 -0500

    Improve header visuals

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,16 +7,21 @@ function Header(props) {
 
     return (
         <header>
-            <nav className="w-100 d-flex justify-content-between align-items-center p-2">
+            <nav className="w-100 d-flex align-items-center p-2">
 
-                <h1 className="mb-0">
+                <img src={`${process.env.PUBLIC_URL}/img/iconTextless.png`} 
+                    className="icon" />
+
+                <h1 className="ml-3 mb-0">
                     {props.league.name ? props.league.name : "Pokédraft Leagues Online"}
                 </h1>
 
-                <DropdownSelector 
-                    setValue={props.setMainContent}
-                    values={options}
-                    purpose={""} />
+                <div className="ml-auto">
+                    <DropdownSelector 
+                        setValue={props.setMainContent}
+                        values={options}
+                        purpose={""} />
+                </div>
 
             </nav>
         </header>
