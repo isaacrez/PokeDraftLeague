98196803d4e417576551962453a553e6a6575ad3
Isaac Rezey <isaacrez20@gmail.com>
2021-02-19 20:58:21 -0500

    Prevent League name from shifting around

--- client/src/components/general/Header.js
+++ client/src/components/general/Header.js
@@ -29,15 +29,15 @@ function Header(props) {
     return (
         <header>
             <nav className="flex flex-column">
-                <div className="w-100 d-flex justify-content-between align-items-center">
+                <div className="w-100 d-flex align-items-center">
                     <button onClick={() => setVisible(v => !v)}
                         className="icon m-1">
                         <img src={`${process.env.PUBLIC_URL}/img/ui/tripleLine.png`} />
                     </button>
 
-                    <h1>{props.league.name}</h1>
+                    <h1 className="ml-3 mb-0">{props.league.name}</h1>
 
-                    <h2 className="mr-3">{props.mainContent}</h2>
+                    <h2 className="ml-auto mr-3 mb-0">{props.mainContent}</h2>
                 </div>
 
                 {visible && buildDropdown()}
