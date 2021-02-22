c7208427786306a57a3059b3fc84e8450669f442
Isaac Rezey <isaacrez20@gmail.com>
2021-02-22 13:21:50 -0500

    Create Card function

--- client/src/components/draftTab/Draft.js
+++ client/src/components/draftTab/Draft.js
@@ -3,13 +3,22 @@ import React from 'react';
 function Draft(props) {
     return (
         <div className="full-stripe">
-            <div className="card">
-                <img src={`https://www.serebii.net/swordshield/pokemon/001.png`} />
-                <p>Bulbasaur</p>
-                <p>FREE</p>
-            </div>
+            <Card imgUrl="https://www.serebii.net/swordshield/pokemon/001.png"
+                title="Bulbasaur"
+                subtitle="FREE" />
+            <Card imgUrl="https://www.serebii.net/swordshield/pokemon/003.png"
+                title="Venusaur"
+                subtitle="LSL" />
         </div>
     )
+}
+
+function Card(props) {
+    return (<div className="card">
+        <img src={props.imgUrl} />
+        <p>{props.title}</p>
+        <p>{props.subtitle}</p>
+    </div>)
 }
 
 export default Draft;
