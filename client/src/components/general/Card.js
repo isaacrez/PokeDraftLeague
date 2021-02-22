c518740f95537ff38ba8a22a10a9d2a3a37ec0e2
Isaac Rezey <isaacrez20@gmail.com>
2021-02-22 17:06:50 -0500

    Add option to specify alt text for Card's image

--- client/src/components/general/Card.js
+++ client/src/components/general/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Card(props) {
     let recolor = props.recolor ? "taken" : null;
     return (<div className={`card mx-1 ${recolor}`}>
-        <img src={props.imgUrl} />
+        <img src={props.imgUrl} alt={props.alt} />
         <p>
             {props.title}
             <br />
