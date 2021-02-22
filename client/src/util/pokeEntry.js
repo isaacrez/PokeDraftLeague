21afeb4b0a0fe88c634522392159f7433f636456
Isaac Rezey <isaacrez20@gmail.com>
2021-02-22 18:51:21 -0500

    Remove obsolete code

--- client/src/util/pokeEntry.js
+++ client/src/util/pokeEntry.js
@@ -46,22 +46,3 @@ function addUpTo(columns, limit) {
     }
     return columns
 }
-
-export const colorizeBy = (string) => intToRGB(hashCode(string));
-
-function hashCode(str) {
-    let hash = 0;
-    for (let i = 0; i < str.length; i++) {
-        hash = str.charCodeAt(i) + ((hash << 12));
-    }
-    return hash;
-}
-
-function intToRGB(i) {
-    let c = (i & 0x00FFFFFF)
-        .toString(16)
-        .toUpperCase();
-
-    return "#00000".substring(0, 7 - c.length) + c;
-}
-
