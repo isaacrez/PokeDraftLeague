f3be0da87c6105ba54a00191f2179b80e4dfb8ff
Isaac Rezey <isaacrez20@gmail.com>
2021-02-23 13:54:35 -0500

    Refactor and remove redundant code

--- client/src/components/summaryTab/PokemonStats.js
+++ client/src/components/summaryTab/PokemonStats.js
@@ -37,33 +37,28 @@ function PokemonStats(props) {
 function Entry(props) {
 
     const [leagueStats, setLeagueStats] = React.useState({});
-    const [loaded, setLoaded] = React.useState(false);
     const KD = leagueStats.directKOs + leagueStats.indirectKOs - leagueStats.deaths;
     const classKD = KD === 0 ? "neutral" : KD > 0 ? "good" : "bad";
 
+    const content = [props.data.tier, leagueStats.gamesPlayed, leagueStats.directKOs,
+                    leagueStats.indirectKOs, leagueStats.deaths];
+    const cells = content.map((s, i) => <td key={i}>{s}</td>)
+
     React.useEffect(() => {
-        setLoaded(false);
         let url = `http://localhost:8080/api/pokemon/stats?pokeId=${props.data.pokemon.id}&leagueId=${props.league.id}`;
         fetch(url, {type: "GET"})
             .then(response => response.json())
             .then(stats => setLeagueStats(stats))
-            .then(() => setLoaded(true))
             .catch(error => console.log(error));
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [props.data.pokemon.id]);
 
 
-    return loaded ? (
-        <tr key={props.data.pokemon.id}>
-            {addLabel(props.data)}
-            <td>{props.data.tier}</td>
-            <td>{leagueStats.gamesPlayed}</td>
-            <td>{leagueStats.directKOs}</td>
-            <td>{leagueStats.indirectKOs}</td>
-            <td>{leagueStats.deaths}</td>
-            <td className={classKD}>{KD > 0 ? `+${KD}` : KD}</td>
-        </tr>
-    ) : null;
+    return (<tr key={props.data.pokemon.id}>
+        {addLabel(props.data)}
+        {cells}
+        <td className={classKD}>{KD > 0 ? `+${KD}` : KD}</td>
+    </tr>);
 }
 
 export default PokemonStats;
