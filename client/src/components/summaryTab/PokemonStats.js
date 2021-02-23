de1bd0727ebb670a66007551a846f3f9032bba01
Isaac Rezey <isaacrez20@gmail.com>
2021-02-23 13:46:56 -0500

    Refactor to improve readability

--- client/src/components/summaryTab/PokemonStats.js
+++ client/src/components/summaryTab/PokemonStats.js
@@ -4,6 +4,11 @@ import { addLabel } from '../../util/pokeEntry';
 function PokemonStats(props) {
 
     const [pokeData, setPokeData] = React.useState([]);
+    const headerLabels = ["Name", "Tier", "Played", "KOs", "Passive", "Deaths", "+/-"];
+
+    const rows = pokeData.sort((a, b) => a.tier - b.tier)
+                .map(data => <Entry league={props.league} data={data} key={data.pokemon.id} />);
+    const header = headerLabels.map(s => <th key={s}>{s}</th>);
 
     React.useEffect(() => {
         let pokemonUrl = `http://localhost:8080/api/pokemon/team?teamId=${props.team.id}&leagueId=${props.league.id}`;
@@ -13,21 +18,12 @@ function PokemonStats(props) {
             .catch(error => console.log(error));
     }, [props.team.id, props.league.id]);
 
-    const rows = pokeData.sort((a, b) => a.tier - b.tier)
-                .map(data => <Entry league={props.league} data={data} key={data.pokemon.id} />)
-
     return (
         <div className="scrollable-table short">
             <table>
                 <thead>
                     <tr>
-                        <th>Name</th>
-                        <th>Tier</th>
-                        <th>Played</th>
-                        <th>KOs</th>
-                        <th>Passive</th>
-                        <th>Deaths</th>
-                        <th>+/-</th>
+                        {header}
                     </tr>
                 </thead>
                 <tbody>
