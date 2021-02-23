249a37ada9d34fb97de55c369de8da6829c5ddcc
Isaac Rezey <isaacrez20@gmail.com>
2021-02-23 15:05:30 -0500

    Simplify logic

--- client/src/components/general/DropdownSelector.js
+++ client/src/components/general/DropdownSelector.js
@@ -2,19 +2,20 @@ import React from 'react';
 
 function DropdownSelector(props) {
 
-    function makeOptions() {
-        let options = props.values.map(v => <option key={v}>{v}</option>);
-        props.DEFAULT && options.unshift(<option key={props.DEFAULT.VALUE} value={props.DEFAULT.VALUE}>
-            {props.DEFAULT.LABEL}
-        </option>)
-        return options;
-    }
+    const defaultOption = props.DEFAULT
+        ? <option key={props.DEFAULT.VALUE} value={props.DEFAULT.VALUE}>{props.DEFAULT.LABEL}</option>
+        : null;
+
+    const options = [
+        defaultOption,
+        props.values.map(v => <option key={v}>{v}</option>)
+    ]
 
     return (
         <div className="minor-dropdown">
             <label htmlFor={props.purpose} className="mr-3">{props.purpose}</label>
             <select id={props.purpose} onChange={e => props.setValue(e.target.value)}>
-                {makeOptions()}
+                {options}
             </select>
         </div>
     );
