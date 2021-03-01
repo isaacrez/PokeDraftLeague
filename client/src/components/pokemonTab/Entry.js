21802dc2c425ff59d53330dec08c2a90f9da173c
Isaac Rezey <isaacrez20@gmail.com>
2021-03-01 17:31:15 -0500

    Team affiliations are viewable

--- client/src/components/pokemonTab/Entry.js
+++ client/src/components/pokemonTab/Entry.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {addLabel, addStats, addTyping, addAbilities, addLeagueStats} from '../../util/pokeEntry';
+import {addLabel, addUpTo} from '../../util/pokeEntry';
 
 function Entry(props) {
 
@@ -21,17 +21,53 @@ function Entry(props) {
 }
 
 function LoadedEntry(props) {
+
+    function addStats() {
+        return Object.values(props.data.stats).map((stat, index) => 
+        <td className="align-middle"
+            key={`${props.data.id}-${index}`}>
+            {stat}
+        </td>);
+    } 
+
+    function addTyping() {
+        return addUpTo(props.data.type.map((type, index) =>
+            <td key={`${props.data.id}-${index}`}>
+                <img src={`https://www.serebii.net/pokedex-bw/type/${type.toLowerCase()}.gif`}
+                    alt={`${type} type`} />
+            </td>
+        ), 2);
+    }
+
+
+    function addAbilities() {
+        addUpTo(props.data.abilities.map((ability, index) =>
+            <td key={`${props.data.id}-${index}`}>
+                {ability}
+            </td>
+        ), 4);
+    }
+
+    function addLeagueStats() {
+        return [
+            <td key={"team"}>{props.data.team.acronym}</td>,
+            <td key={"directKOs"}>{props.leagueStats.directKOs}</td>,
+            <td key={"indirectKOs"}>{props.leagueStats.indirectKOs}</td>,
+            <td key={"deaths"}>{props.leagueStats.deaths}</td>
+        ];
+    }
+
     const displayOptions = {
-        "Base Stats": addStats(props.data),
-        "Typing": addTyping(props.data),
-        "Abilities": addAbilities(props.data),
-        "League Stats": addLeagueStats(props.leagueStats)
+        "Base Stats": addStats,
+        "Typing": addTyping,
+        "Abilities": addAbilities,
+        "League Stats": addLeagueStats
     }
     
     const cells = [
         addLabel(props.data),
         Object.keys(props.display)
-            .map(d => props.display[d] ? displayOptions[d] : null)
+            .map(d => props.display[d] ? displayOptions[d]() : null)
     ];
 
     const recolor = props.data.team.acronym !== "FREE" ? "taken" : "";
--- client/src/util/pokeEntry.js
+++ client/src/util/pokeEntry.js
@@ -8,39 +8,7 @@ export const addLabel = data =>
         <p>{data.name}</p>
     </td>);
 
-export const addStats = data => 
-    Object.values(data.stats).map((stat, index) => 
-        <td className="align-middle"
-            key={`${data.id}-${index}`}>
-            {stat}
-        </td>);
-
-export const addTyping = data =>
-    addUpTo(data.type.map((type, index) =>
-        <td key={`${data.id}-${index}`}>
-            <img src={`https://www.serebii.net/pokedex-bw/type/${type.toLowerCase()}.gif`}
-                alt={`${type} type`} />
-        </td>
-    ), 2);
-
-
-export const addAbilities = data =>
-     addUpTo(data.abilities.map((ability, index) =>
-        <td key={`${data.id}-${index}`}>
-            {ability}
-        </td>
-    ), 4);
-
-export const addLeagueStats = data =>
-    [
-        <td key={"team"}>{data.team ? data.team.acronym : "FREE"}</td>,
-        <td key={"directKOs"}>{data.directKOs}</td>,
-        <td key={"indirectKOs"}>{data.indirectKOs}</td>,
-        <td key={"deaths"}>{data.deaths}</td>
-    ];
-
-
-function addUpTo(columns, limit) {
+export const addUpTo = (columns, limit) => {
     while (columns.length < limit) {
         columns.push(<td key={columns.length} />)
     }
